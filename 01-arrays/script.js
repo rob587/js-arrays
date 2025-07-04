9fea50a1e64225c20d7980f0e41900459396f782
rob587 <robertocamm02@yahoo.com>
2025-07-04 09:41:18 +0200

    [fix]

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,8 +10,8 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = teachers[4]
-console.log(teachers[4]);
+const fourthTeacher = teachers[3]
+console.log(teachers[3]);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
